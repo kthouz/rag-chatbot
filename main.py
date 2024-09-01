76232ea091fad3e33ea32696af22b1b4835effae
kthouzwudz <kthouz@redwoods.ai>
2024-08-31 20:46:55 -0700

    port

--- main.py
+++ main.py
@@ -149,4 +149,4 @@ def generate(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
