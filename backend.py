db4d9eaab9bf07393af79fc3a61ed6ff4a41a33f
kthouzwudz <kthouz@redwoods.ai>
2024-08-31 21:10:49 -0700

    cleanup

--- backend.py
+++
@@ -1,9 +0,0 @@
-from src import index
-
-async def index_document(path: str, collection: str): 
-    # Logic to index the document
-    doc = index.Document(path, collection)
-    doc.load_document()
-    doc.index_document()
-    doc.save_metadata()
-    pass
