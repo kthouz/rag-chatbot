9ec9a2f8f830100be24ad8f40bbc3677241cbbf0
kthouzwudz <kthouz@redwoods.ai>
2024-09-02 13:22:41 -0700

    fix

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_NAME = os.get_env("DB_NAME", "test")
+DB_NAME = os.getenv("DB_NAME", "test")
 DB_CONNECTION_STRING = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 DOCS_BUCKET = os.getenv("DOCS_BUCKET", "docs")
